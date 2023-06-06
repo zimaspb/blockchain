76d36d7262d553ba99c96145374bbc24009c5acd
zimaspb <135603526+zimaspb@users.noreply.github.com>
2023-06-06 10:09:19 +0300

    lib.sol

--- lib.sol
+++ lib.sol
@@ -2,13 +2,14 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Library {
-    constructor() {
-        LibAdmin=msg.sender;
+
+contract Library{
+    constructor(){
+        libAdmin=msg.sender;
     }
 
-    uint amountBooks = 0;
-    address LibAdmin;
+    uint public amountBooks = 0;
+    address libAdmin;
     uint public priceForMonth = 1000 gwei;
 
     struct Book {
@@ -16,59 +17,58 @@ contract Library {
         string picture;
         bool availability;
     }
-    
+
     mapping (uint => Book) bookNumber;
     mapping (uint => address) rentedTo;
 
-   //----------Admin
+    //------------Admin
 
-   function changeAdmin(address _newAdmin) public {
-       require (LibAdmin==msg.sender, "only admin");
-       LibAdmin = _newAdmin;
-   }
-   
-   function withdraw() public { 
-        require(LibAdmin==msg.sender, "Only admin"); 
-        payable(LibAdmin).transfer(address(this).balance); 
+    function changeAdmin(address _newAdmin) public {
+        require(libAdmin==msg.sender, "Only admin");
+        libAdmin = _newAdmin;
     }
 
+    function withdraw() public {
+        require(libAdmin==msg.sender, "Only admin");
+        payable(libAdmin).transfer(address(this).balance);
+    }
 
-   //----------Book
+    //------------Book
 
-   function createBook(string calldata _name, string calldata _image) public returns (uint) {
-        require (LibAdmin==msg.sender, "only admin");
+    function createBook(string calldata _name, string calldata _image) public {
+        require(libAdmin==msg.sender, "Only admin");
         //book creation
-        bookNumber[amountBooks] = Book (_name, _image, true);
+        bookNumber[amountBooks] = Book(_name, _image, true);
         amountBooks++;
-        return amountBooks-1;
-
-   }
-    //----------Information about books
-
-    function bookInfo(uint _bookID) public view returns (Book memory) {
-        require (_bookID<amountBooks, "Not exist");
-        return bookNumber [_bookID];
+        //return amountBooks--;
 
     }
-    //----------Rent
-    function rentBook(uint _bookID, uint _month) public payable {
-        require (_bookID<amountBooks, "Not exist");
-        require (priceForMonth*_month==msg.value, "Not enough funds");
-        require(rentedTo[_bookID]==0x0000000000000000000000000000000000000000, "Already rented");
+
+    function bookInfo(uint _bookID) public view returns ( Book memory){
+        // require(_bookID<amountBooks, "Not exist");
+        return bookNumber[_bookID];
+    }
+
+    function rentBook(uint _bookID, uint _month) public payable{
+        require(_bookID<amountBooks, "Not exist");
+        require(priceForMonth*_month==msg.value, "Not enough funds");
+       // require(rentedTo[_bookID]==0x0000000000000000000000000000000000000000, "Already rented");
+        require(bookNumber[_bookID].availability, "Already rented");
         rentedTo[_bookID] = msg.sender;
         bookNumber[_bookID].availability = false;
-
     }
-    function whereIsBook (uint _bookID) public view returns (address){
-         require (_bookID<amountBooks, "Not exist");
-         return rentedTo[_bookID];
 
+    function whereIsBook(uint _bookID) public view returns(address){
+        require(_bookID<amountBooks, "Not exist");
+        return rentedTo[_bookID];
     }
-    function returnBook (uint _bookID) public {
-        require (msg.sender==LibAdmin || msg.sender==rentedTo[_bookID], "Only for admin");
+
+    function returnBook(uint _bookID) public {
+        require(msg.sender==libAdmin || msg.sender ==rentedTo[_bookID], "Only admin");
         bookNumber[_bookID].availability = true;
         delete rentedTo[_bookID];
-
     }
+
+
 
 }
